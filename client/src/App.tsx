db85614aedcb47104812f3c8f5d9c7679e2b523c
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-05-04 11:33:19 +0900

    requests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import ClientList from "./components/ClientList";
+import ErrorBoundary from "./ErrorBoundary";
 
 function App() {
   return (
     <div className="App">
-      <ClientList />
+      <div>sample application</div>
+      <ErrorBoundary>
+        <ClientList />
+      </ErrorBoundary>
     </div>
   );
 }
