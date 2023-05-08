bdd27647d60ca5e6e7b03c7c4813ba4edc2d0cbe
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-05-08 20:07:03 +0900

    save deal

--- client/src/components/DealEditor.tsx
+++ client/src/components/DealEditor.tsx
@@ -4,15 +4,26 @@ import { Client, ClientWithDeals, Deal } from "../api/types";
 
 function clientOption(client: ClientWithDeals): JSX.Element {
   return (
-    <option key={client.id} value={client.id}>
+    <option key={client.id} value={client.id.toString()}>
       {client.name}
     </option>
   );
 }
 
+function maybeInt(s: string): number {
+  let ret = parseInt(s);
+  if (isNaN(ret)) {
+    throw new Error(`can not parse as int ${s}`);
+  }
+  return ret;
+}
+
 function DealEditor(): JSX.Element {
   const [clients, setClients] = useState<ClientWithDeals[]>([]);
-  const [obj, setObj] = useState<Partial<Deal>>({ clientId: undefined });
+  const [obj, setObj] = useState<Partial<Deal>>({
+    clientId: undefined,
+    name: "",
+  });
 
   useEffect(() => {
     getAllClients().then(setClients);
@@ -30,14 +41,37 @@ function DealEditor(): JSX.Element {
     <div>
       <label>
         client
-        <select style={{ margin: "10px" }} value={obj.clientId ?? ""}>
+        <select
+          style={{ margin: "10px" }}
+          value={obj.clientId ?? ""}
+          onChange={(e) => {
+            const v = e.target.value;
+            setObj({
+              ...obj,
+              clientId: v === "" ? undefined : maybeInt(v),
+            });
+          }}
+        >
           {options}
         </select>
       </label>
       <label>
         name
-        <input style={{ margin: "10px" }} />
+        <input
+          style={{ margin: "10px" }}
+          value={obj.name}
+          onChange={(e) => {
+            setObj({ ...obj, name: e.target.value });
+          }}
+        />
       </label>
+      <button
+        onClick={() => {
+          console.log("obj", obj);
+        }}
+      >
+        save
+      </button>
     </div>
   );
 }
